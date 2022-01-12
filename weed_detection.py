ff402b76e37a3b282694b5fb4179d1575e3cb0f4
giorgos kalomitsinis <giorgoskalomitsinis@gmail.com>
2022-01-12 16:43:51 +0200

    Update weed_detection.py

--- weed_detection.py
+++ weed_detection.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow import keras
 import segmentation_models as sm
 import glob
 import cv2
@@ -8,7 +7,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 import datetime 
 import sys
+from tensorflow import keras
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # Optional for hiding the tensorflow messages
 
 def winapi_path(dos_path, encoding=None):
 	if (not isinstance(dos_path, str) and encoding is not None): 
@@ -22,12 +23,15 @@ images = winapi_path(sys.argv[1])
 save_dir = winapi_path(sys.argv[2])
 results_dir = os.path.join(save_dir, 'results')
 os.makedirs(results_dir, exist_ok=True)
+sm.set_framework('tf.keras')
 
-
+extensions = ["*.png", "*.jpg", ".jpeg"]
 start_sc = datetime.datetime.now()
+
+original_shapes = []
 predict_imgs = []
-for directory_path in glob.glob(images):
-	for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+for ext in extensions:
+	for img_path in glob.glob(os.path.join(images, ext)):
 		img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 		height, width, channels = img.shape
 
@@ -43,14 +47,12 @@ for directory_path in glob.glob(images):
 		else:
 			new_width = width
 
-		result = cv2.copyMakeBorder(img, int((new_height-height)/2), int((new_height-height)/2), int((new_width-width)/2), int((new_width-width)/2), cv2.BORDER_CONSTANT, None, value = 0)
-		img = cv2.cvtColor(result, cv2.COLOR_BGR2RGB) 
-		predict_imgs.append(img)
+		result = cv2.copyMakeBorder(img, 0, int(new_height-height), 0, int(new_width-width), cv2.BORDER_CONSTANT, None, value = 0)
+		img_reshaped = cv2.cvtColor(result, cv2.COLOR_BGR2RGB) 
+		original_shapes.append(img.shape)
+		predict_imgs.append(img_reshaped)
 	   
-
 predict_imgs_arr = np.array(predict_imgs)
-
-sm.set_framework('tf.keras')
 
 classes = ['weeds', 'background']
 activation = 'sigmoid' if len(classes) == 1 else 'softmax'
@@ -62,10 +64,8 @@ model = sm.Unet(BACKBONE, classes=len(classes), activation=activation)
 model.load_weights('./weights0300.hdf5')
 
 os.chdir(results_dir)
-c = 0
 times = []
-for i, pred_img in enumerate(predict_imgs_arr):
-	c += 1
+for i, (pred_img, original_shape) in enumerate(zip(predict_imgs_arr, original_shapes)):
 	pred_img_input = np.expand_dims(pred_img, 0)
 	test_img_input = preprocess_input(pred_img_input)
 
@@ -74,26 +74,25 @@ for i, pred_img in enumerate(predict_imgs_arr):
 	stop = datetime.datetime.now()
 	times.append((stop-start).total_seconds())
 	
-	mask = np.argmax(mask_unet, axis=3)[0,:,:]*255
-	
+	mask = np.argmax(mask_unet, axis=3)[0,:,:]*255	
 	mask_3ch = np.stack((mask, )*3, axis = -1)
 	mask_3ch[mask==255] = [51, 153, 255]
 	mask_3ch = mask_3ch.astype('int8')
 
 	result = cv2.addWeighted(pred_img, 1, mask_3ch, 0.7, 0.7, dtype = cv2.CV_8UC2)
-
+	result_reshaped = result[:original_shape[0], :original_shape[1]]
 
 	f = plt.figure()
-	f.set_figheight(result.shape[0] / f.get_dpi())
-	f.set_figwidth(result.shape[1] / f.get_dpi())
+	f.set_figheight(result_reshaped.shape[0] / f.get_dpi())
+	f.set_figwidth(result_reshaped.shape[1] / f.get_dpi())
 	ax = plt.Axes(f, [0., 0., 1., 1.])
 	ax.set_axis_off()
 	f.add_axes(ax)
-	ax.imshow(result)
+	ax.imshow(result_reshaped)
 	f.savefig('ID_{}.png'.format(i))
 	plt.close()
 
 stop_sc = datetime.datetime.now()
-print('Average prediction time for each image: {}s'.format(sum(times[1:])/c))
+print('Average prediction time for each image: {}s'.format(sum(times[1:])/len(times)))
 print('Execution time of this module: {}s'.format(stop_sc-start_sc))
 print('Done!')
